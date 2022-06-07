cece63a76116af1f6db27ace600253109efdb20e
trapido02 <trapido02.2@gmail.com>
2022-06-07 11:25:59 +0200

    Added toggle for streaming mode.

--- src/main/java/com/trapido02/Streamtools.java
+++ src/main/java/com/trapido02/Streamtools.java
@@ -1,21 +1,35 @@
 package com.trapido02;
 
 import com.trapido02.commands.Live;
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
+
+import net.luckperms.api.LuckPerms;
+import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
 public final class Streamtools extends JavaPlugin implements Listener {
     private static Streamtools instance;
+    public static LuckPerms api = null;
+
+    public static Logger logger = LoggerFactory.getLogger(Streamtools.class);
 
     @Override
     public void onEnable() {
         instance = this;
 
         System.out.println("Starting Streamtools!");
+
+
+        RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+        if (provider != null) {
+            api = (LuckPerms) ((RegisteredServiceProvider<?>) provider).getProvider();
+        }
 
         // Prevent "NullPointerException"
         Objects.requireNonNull(getCommand("stream")).setTabCompleter(new Live());
--- src/main/java/com/trapido02/commands/Live.java
+++ src/main/java/com/trapido02/commands/Live.java
@@ -3,6 +3,9 @@ package com.trapido02.commands;
 import com.trapido02.Message;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.luckperms.api.model.data.DataMutateResult;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.types.InheritanceNode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,6 +18,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static com.trapido02.Streamtools.api;
+
 public class Live implements CommandExecutor, TabCompleter {
     enum Platforms {
         TWITCH,
@@ -22,7 +27,9 @@ public class Live implements CommandExecutor, TabCompleter {
         DISCORD
     }
 
-    private static final String[] COMMANDS = { "TWITCH", "YOUTUBE", "DISCORD" };
+    private static final String[] COMMANDS = { "TWITCH", "YOUTUBE", "DISCORD", "STOP" };
+
+    private static boolean isStreaming = false;
 
     public static boolean contains(String v) {
         for (Platforms c : Platforms.values()) {
@@ -53,35 +60,77 @@ public class Live implements CommandExecutor, TabCompleter {
             return true;
         }
 
+        // Luckperms initialize user for setting node
+        String group = "Producer";
+        User user = api.getPlayerAdapter(Player.class).getUser(player);
+
         // Display the proper display name
         Component comp = ((Player) sender).displayName();
 
-        if (args.length == 1 && contains(args[0])) {
-            Message.sendServer(sender, comp.append(Component
-                    .text(" has started streaming on " + args[0].toUpperCase() + "!"))
-                    .color(NamedTextColor.DARK_PURPLE)
-            );
+        // Check if player tries to run stop argument
+        if (args[0].equalsIgnoreCase("stop")) {
+            if (isStreaming) {
+                InheritanceNode node = InheritanceNode.builder(group).value(false).build();
+                DataMutateResult result = user.data().add(node);
 
-            // If the user didn't provide with a Discord invite, default it to the Gulag
-            if (args[0].equalsIgnoreCase("discord")) {
-                Message.senderServerURL(sender, "[JOIN GULAG]", "https://discord.gg/BUNxGdVerg");
+                Message.sendServer(sender, comp.append(Component
+                        .text(" has STOPPED streaming!"))
+                        .color(NamedTextColor.GREEN)
+                );
+                Message.sendClient(player, "Stopped streaming!");
+
+                isStreaming = false;
+            } else {
+                Message.sendClient(sender, "You aren't streaming.");
+                return true;
+            }
+
+            return true;
+        }
+
+        if (args.length == 1 && contains(args[0])) {
+            if (!isStreaming) {
+                Message.sendServer(sender, comp.append(Component
+                        .text(" has started streaming on " + args[0].toUpperCase() + "!"))
+                        .color(NamedTextColor.DARK_PURPLE)
+                );
+
+                // If the user didn't provide with a Discord invite, default it to the Gulag
+                if (args[0].equalsIgnoreCase("discord")) {
+                    Message.senderServerURL(sender, "[JOIN GULAG]", "https://discord.gg/BUNxGdVerg");
+                }
+
+                isStreaming = true;
+            } else {
+                Message.sendClient(sender, "You are already streaming.");
             }
 
             return true;
         }
 
         if (args.length == 2 && contains(args[0])) {
-            Message.sendServer(sender, comp.append(Component
-                    .text(" has started streaming on " + args[0].toUpperCase() + "!"))
-                    .color(NamedTextColor.DARK_PURPLE)
-            );
+            if (args[0].equalsIgnoreCase("stop")) { return true; }
 
-            if (args[0].equalsIgnoreCase("discord")) {
-                Message.senderServerURL(sender, "[JOIN DISCORD]", "https://discord.com/" + args[1]);
-            } else if (args[0].equalsIgnoreCase("twitch")) {
-                Message.senderServerURL(sender, "[JOIN STREAM]", "https://twitch.tv/" + args[1]);
-            } else if (args[0].equalsIgnoreCase("youtube")) {
-                Message.senderServerURL(sender, "[JOIN STREAM]", "https://youtube.com/" + args[1]);
+            if (!isStreaming) {
+                Message.sendServer(sender, comp.append(Component
+                        .text(" has started streaming on " + args[0].toUpperCase() + "!"))
+                        .color(NamedTextColor.DARK_PURPLE)
+                );
+
+                if (args[0].equalsIgnoreCase("discord")) {
+                    Message.senderServerURL(sender, "[JOIN DISCORD]", "https://discord.com/" + args[1]);
+                } else if (args[0].equalsIgnoreCase("twitch")) {
+                    Message.senderServerURL(sender, "[JOIN STREAM]", "https://twitch.tv/" + args[1]);
+                } else if (args[0].equalsIgnoreCase("youtube")) {
+                    Message.senderServerURL(sender, "[JOIN STREAM]", "https://youtube.com/" + args[1]);
+                }
+
+                InheritanceNode node = InheritanceNode.builder(group).value(true).build();
+                DataMutateResult result = user.data().add(node);
+
+                isStreaming = true;
+            } else {
+                Message.sendClient(sender, "You are already streaming.");
             }
 
             return true;
